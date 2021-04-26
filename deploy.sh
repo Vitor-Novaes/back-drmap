3e76571a7ee95a7bba5df8e04754b19aab856cad
Vitor Novaes <vitornovaes27@gmail.com>
2021-04-26 10:03:34 -0300

    Gitignore updates

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin http://734146913661.dkr.ecr.us-east-1.amazonaws.com/drm
-sudo docker build -t drm_api .
-sudo docker tag drm_api:latest 734146913661.dkr.ecr.us-east-1.amazonaws.com/drm:latest
-sudo docker push 734146913661.dkr.ecr.us-east-1.amazonaws.com/drm:latest
-
-
